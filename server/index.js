19701979b8ec86588b18337556b8b42771490449
pawan singhla <pawansinghla300@gmail.com>
2020-11-27 19:25:34 +0530

    add_post

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 
-const CONNECTION_URL="mongodb+srv://pawan:pawan@cluster0.thk2a.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const CONNECTION_URL="mongodb+srv://pawan:pawan@cluster0.thk2a.mongodb.net/blogpost?retryWrites=true&w=majority"
 const PORT=process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(()=>app.listen(PORT,()=>console.log(`Server running on  port: ${PORT}`)))
